dbdccb8ce4ccb6559f0b69ba41e42a888fa5b689
suchi5533 <suchitw867@gmail.com>
2026-04-16 12:13:17 +0530

    first commit

---
+++ src/main/java/com/ats/warehouse/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.ats.warehouse.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+@GetMapping("/test")
+public String testApi()
+{
+    return "warehouse management system";
+}
+
+}
