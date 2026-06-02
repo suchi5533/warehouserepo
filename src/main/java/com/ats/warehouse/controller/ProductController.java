f4d17a4af45e62264343135626c60e1a3e741cbe
suchi5533 <suchitw867@gmail.com>
2026-06-02 09:20:35 +0530

    ngrok updated

--- src/main/java/com/ats/warehouse/controller/ProductController.java
+++ src/main/java/com/ats/warehouse/controller/ProductController.java
@@ -1,11 +1,24 @@
 package com.ats.warehouse.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
 
+	@PostMapping("/github-webhook")
+    public ResponseEntity<String> githubWebhook(
+            @RequestBody(required = false) String payload) {
+
+        System.out.println("===== GitHub Webhook Received =====");
+        System.out.println(payload);
+
+        return ResponseEntity.ok("Webhook received successfully");
+    }
+	
 @GetMapping("/test")
 public String testApi()
 {
